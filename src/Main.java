b808d319f2869e34f3dec24727bb7a3eb30a6e6d
Sameer Khokhar <skkhokhar787@gmail.com>
2025-07-05 16:33:23 +0500

    Add Car_code 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Car {
     String make;
     String model;
@@ -19,53 +21,56 @@ class Car {
         System.out.println();
     }
 }
-class ElectricCar extends Car {
-    int batteryLevel;
 
-    public void electricCar(String make, String model, int year){
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        //this.batteryLevel = batteryLevel;
-    }
+class ElectricCar extends Car {
+    Scanner bc = new Scanner(System.in);
+    int batteryLevel;
 
     public ElectricCar(String make, String model, int year, int batteryLevel) {
         super(make, model, year);
         this.batteryLevel = batteryLevel;
     }
 
-    @Override
-    public void startEngine () {
+
+    public void startEngine() {
         if (batteryLevel > 20) {
-            System.out.println(make+" "+model+" "+"("+year+") is silently power on...");
-        }else {
-            System.out.println(make+" "+model+" "+"("+year+") Low battery ! Please charge...");
+            System.out.println(make + " " + model + " (" + year + ") is silently power on...");
+            System.out.println(make + " " + model + " (" + year + ") engine stoped..");
             System.out.println();
+        } else {
+            System.out.println(make + " " + model + " (" + year + ") Low battery! Please charge...");
+            System.out.println("To charge battery.. Enter \"charge battery\"");
+            String inputChargeBattery = bc.nextLine();
+            if (inputChargeBattery.equals("charge battery")) {
+                System.out.println("....charging on....");
+            }
         }
     }
-    public void chargeBattery () {
+
+    public void chargeBattery() {
+
         batteryLevel = 100;
-        System.out.println(model+" "+"("+year+") battery is charged to "+batteryLevel+"%");
+        System.out.println(model + " (" + year + ") battery is charged to " + batteryLevel + "%");
     }
 }
 
-
 public class Main {
     public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
         Car petrolCar = new Car("Toyota","Corolla",2020);
-        int batteryLevel = 35;
+        System.out.print("Enter Tesla battery level : ");
+        int batteryLevel = sc.nextInt();
         ElectricCar tesla = new ElectricCar("Tesla","Model 3",2025, batteryLevel);
 
         petrolCar.startEngine();
         petrolCar.stopEngine();
 
-        if (batteryLevel <= 20) {
-            System.out.println(" Low battery! Please charge...");
-            System.out.println();
+        if (batteryLevel > 20) {
+            tesla.startEngine();
+        }else {
+            tesla.startEngine();
             tesla.chargeBattery();
         }
-        tesla.startEngine();
-        tesla.stopEngine();
 
     }
 }
